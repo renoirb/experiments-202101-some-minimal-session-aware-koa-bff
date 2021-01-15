08733afe1eb63aa3a216fd28a7790e737b596dca
Renoir Boulanger <contribs@renoirboulanger.com>
2021-01-15 12:47:54 -0300

    Proxy server-sent-events

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [repo]:
   https://github.com/renoirb/experiments-202101-some-minimal-session-aware-koa-bff/branches
 
-# [Experiments **2020-01**; Some minimal session aware Backend For Frontned using Koa][repo]
+# [Experiments **2021-01**; Some minimal session aware Backend For Frontned using Koa][repo]
 
 In order to allow a stateless web app that gives more context to a
 micro-frontend, we need an HTTP servie that can tell us if we're authenticated
@@ -14,3 +14,20 @@ along an authoritative source.
 For this, we can make this BFF store for us this _authentication proof_ as an
 HTTPOnly cookie that this BFF can re-use, so that client-side JavaScript never
 sees it.
+
+## Usage example
+
+Before starint the service, setup options
+
+```
+export APP_CONSOLE_LOG=please
+export APP_DATA_SOURCE_ORIGIN_PROXY="https://foo.bar.some.origin.example.org/some/path"
+yarn start
+```
+
+Then, in a web browser, we'll have to tell which `Authoriztion: Bearer ...` to
+use, we can tell it by using the _recovery_ route
+
+- Look the current autorization token `http://localhost:3000/bff/whois`
+- Change the authorization token `http://localhost:3000/bff/recovery`
+- Make the request to the proxyed service `http://localhost:3000/bff/proxy`
--- index.mjs
+++ index.mjs
@@ -1,13 +1,12 @@
 import Koa from 'koa'
 import koaBody from 'koa-body'
 import session from 'koa-session'
+import koaHttProxy from 'koa-better-http-proxy'
 import {
   hasSessionCookieMiddleware,
   recoveryFormMiddleware,
-  createSessionState,
 } from '@renoirb/koa-middleware-utils'
 
-const AS_IF_WE_VALIDATED = '111.222.333'
 const SESSION_KEY = 'bff'
 
 /** @type {import('@renoirb/koa-middleware-utils').IHasSessionCookieMiddlewareOptions} */
@@ -21,10 +20,42 @@ const CONFIG_SESSION_RECOVERY = {
   redirect: '/bff/whois',
 }
 
+const {
+  // BEGIN Node.js/npm managed environment varibles from package.json
+  npm_package_name = 'bff',
+  // END Node.js/npm managed environment varibles from package.json
+  // BEGIN environment variables we can set prior to running
+  // set to anything else to avoid console.log
+  APP_CONSOLE_LOG = false,
+  APP_CONSOLE_LOG_ERROR = false,
+  APP_SESSION_SECRET_KEYS = 'replace,me,please',
+  APP_DATA_SOURCE_ORIGIN_PROXY = null,
+  // END environment variables we can set prior to running
+} = process.env || {}
+
+/**
+ * Really, we should check a bit more about the string
+ * what it contains.
+ * This isn't going to check if it is valid.
+ * For this we'd need the Session State middleware to do more.
+ */
+const isValid = (input) => typeof input === 'string'
+
+// tip: koa-bunyan, koa-bunyan-logger, koa-logger
+const log = (...args) =>
+  APP_CONSOLE_LOG !== false
+    ? console.log.apply(null, [npm_package_name, ...args])
+    : void 0
+
+const logError = (...args) =>
+  APP_CONSOLE_LOG_ERROR === false
+    ? console.log.apply(null, [`${npm_package_name} ERROR`, ...args])
+    : void 0
+
 // BEGIN Example
 // https://github.com/koajs/session#example
 const app = new Koa()
-app.keys = ['some secret hurr']
+app.keys = (APP_SESSION_SECRET_KEYS || '').split(',')
 app.use(
   session(
     {
@@ -39,8 +70,57 @@ app.use(koaBody())
 // END Example
 
 // Only routes starting by baseApi
+// https://github.com/nsimmons/koa-better-http-proxy
+const proxyAlphaUrl = `${CONFIG_SESSION_RECOVERY.baseApi}/proxy`
+if (APP_DATA_SOURCE_ORIGIN_PROXY) {
+  app.use(
+    koaHttProxy(APP_DATA_SOURCE_ORIGIN_PROXY, {
+      timeout: 2000,
+      strippedHeaders: ['set-cookie', 'location'],
+      filter(ctx) {
+        // Contrived example of only one, normally we would have more thn one
+        const proxyAlpha = ctx.path.startsWith(proxyAlphaUrl)
+        let proxy = proxyAlpha !== false
+        log('koaHttpProxy filter', {
+          proxyAlphaUrl,
+          proxyAlpha,
+          shouldProxy: proxy,
+        })
+        ctx.state.proxy = proxy
+        return proxy
+      },
+      // https://www.npmjs.com/package/koa-better-http-proxy#proxyreqoptdecorator--supports-promise-form
+      proxyReqOptDecorator(proxyReqOpts, ctx) {
+        const authorizationToken =
+          ctx.session &&
+          ctx.session.sessionState &&
+          ctx.session.sessionState.authorizationToken
+        ctx.assert(
+          typeof authorizationToken === 'string' && authorizationToken !== '',
+          401,
+        )
+        if (authorizationToken) {
+          proxyReqOpts.headers['Authorization'] = `Bearer ${authorizationToken}`
+        }
+        const headers = {
+          ...proxyReqOpts.headers,
+        }
+        log('koaHttpProxy proxyReqOptDecorator', {
+          headers,
+          authorizationToken,
+        })
+        return proxyReqOpts
+      },
+    }),
+  )
+  console.log(`Will Proxy ${proxyAlphaUrl} => ${APP_DATA_SOURCE_ORIGIN_PROXY}`)
+} else {
+  console.log(`Will NOT Proxy ${proxyAlphaUrl}`)
+}
+
+// Only routes starting by baseApi
 app.use((ctx, next) => {
-  console.log(`BEGIN\t${ctx.method} ${ctx.url}`, {
+  log(`BEGIN\t${ctx.method} ${ctx.url}`, {
     path: ctx.path,
     baseApi: CONFIG_SESSION_RECOVERY.baseApi,
     test: ctx.path.startsWith(CONFIG_SESSION_RECOVERY.baseApi),
@@ -49,25 +129,29 @@ app.use((ctx, next) => {
     // It starts by our prefix, carry on.
     next()
   }
-  console.log(`END\t${ctx.method} ${ctx.url}`)
+  log(`END\t${ctx.method} ${ctx.url}`)
 })
 
-
+// Session State middlewre
 app.use((ctx, next) => {
-  let sessionState = { jwt: '' }
+  let sessionState = { authorizationToken: '' }
   if (ctx.state.sessionState) {
-    sessionState = JSON.parse(JSON.stringify({ jwt: '', ...ctx.state.sessionState }))
+    sessionState = JSON.parse(
+      JSON.stringify({ authorizationToken: '', ...ctx.state.sessionState }),
+    )
   }
+
   if (
     ctx.path ===
     `${CONFIG_SESSION_RECOVERY.baseApi}${CONFIG_SESSION_RECOVERY.recoveryPath}`
   ) {
     if (ctx.method === 'POST') {
-      if (AS_IF_WE_VALIDATED === ctx.request.body.jwt) {
+      const { jwt = null } = ctx.request.body
+      if (isValid(jwt)) {
         // This should come from a source of validation, and source of truth
-        // Adding to the state the jwt, but we could also keep other preferences
-        const sessionStateNew = { jwt: '', ...sessionState }
-        sessionStateNew.jwt = ctx.request.body.jwt
+        // Adding to the state the authoriztionToken, but we could also keep other preferences
+        const sessionStateNew = { authorizationToken: '', ...sessionState }
+        sessionStateNew.authorizationToken = jwt
         sessionState = sessionStateNew
       }
       // Persisting to the session (i.e. creating cookie) as per how koa-session is configured.
@@ -76,50 +160,43 @@ app.use((ctx, next) => {
       return
     }
   }
-  // ------ END Things that would be in koa routes -----
-
-  let newSessionState = JSON.parse(JSON.stringify(sessionState))
 
   // ------ BEGIN Sharing session state ------
-
+  // But, really, there has to be a simpler way. TODO
+  let newSessionState = JSON.parse(JSON.stringify(sessionState))
   // 1. Either pick from cookie, thanks koa-session!
   let sessionStateMethod1 = null
   try {
     const maybe = JSON.parse(JSON.stringify(ctx.session.sessionState))
     sessionStateMethod1 = maybe
   } catch (e) {
-    console.log('Sharing sessions state, method 1 ERROR', e)
+    logError('Sharing sessions state, method 1 failed', e)
   }
-
-  // 1. Or do it yourself, manually
+  // 2. Or do it yourself, manually
   let sessionStateMethod2 = null
   try {
     const cookieContentsString = ctx.cookies.get(CONFIG_HAS_SESSION.cookieName)
-    const buff = Buffer.from(cookieContentsString, 'base64')
+    const buff = Buffer.from(cookieContentsString || '', 'base64')
     const cookieContents = buff.toString('ascii')
     const maybe = JSON.parse(cookieContents)
     sessionStateMethod2 = maybe && maybe.sessionState ? maybe.sessionState : {}
   } catch (e) {
-    console.log('Sharing sessions state, method 2 ERROR', e)
-    // ...
+    logError('Sharing sessions state, method 2 failed', e)
   }
-
-  // 2. Or use hasSessionCookie from hasSessionCookie middleware
+  // 3. Or use hasSessionCookie from hasSessionCookie middleware
   let sessionStateMethod3 = null
   try {
     const fromHasSessionCookieString = ctx.cookies.get(
       CONFIG_HAS_SESSION.cookieName,
     )
-    const buff2 = Buffer.from(fromHasSessionCookieString, 'base64')
+    const buff2 = Buffer.from(fromHasSessionCookieString || '', 'base64')
     const fromHasSessionCookieContents = buff2.toString('ascii')
     const maybe = JSON.parse(fromHasSessionCookieContents)
     sessionStateMethod3 = maybe && maybe.sessionState ? maybe.sessionState : {}
   } catch (e) {
-    console.log('Sharing sessions state, method 3 ERROR', e)
-    // ...
+    logError('Sharing sessions state, method 3 failed', e)
   }
-
-  console.log(
+  log(
     `END\t${ctx.method} ${ctx.url}\tController-ish Sharing session state methods`,
     {
       sessionStateMethod1,
@@ -127,7 +204,6 @@ app.use((ctx, next) => {
       sessionStateMethod3,
     },
   )
-
   // Use one of the three methods
   const fallback = JSON.parse(JSON.stringify(newSessionState))
   newSessionState =
@@ -138,26 +214,35 @@ app.use((ctx, next) => {
   // sessionState = sessionStateMethod1
   // sessionState = sessionStateMethod2
   // sessionState = sessionStateMethod3
-
   // Ensure the ctx.state (koa internal state for cross-middleware state) is the same as ctx.session (cookie)
   ctx.state.sessionState = JSON.parse(JSON.stringify(newSessionState))
-
   // ------ END Sharing session state ------
 
   next()
 
   if (ctx.path === `${CONFIG_SESSION_RECOVERY.baseApi}/whois`) {
-    ctx.body = ( ctx.state.sessionState || {} )
-    // Nothing else to do, we stop here — no next!
-    return
+    let body = {}
+    try {
+      // Really, we should not leak jwt at all.
+      // This is just for show.
+      // Ideally, we should provide a consistent current user state
+      // with things such as locale, timezone, default user data fullName, etc.
+      const maybe = JSON.parse(JSON.stringify(ctx.state.sessionState || {}))
+      body = {
+        __comment__:
+          'This is just for show. Normally authorizationToken should be communicated exclusively via HTTPOnly cookies',
+        ...maybe,
+      }
+    } catch (e) {
+      logError(e)
+    }
+    ctx.body = body
   }
 
-  console.log(`END\t${ctx.method} ${ctx.url}\tController-ish`, {
+  log(`END\t${ctx.method} ${ctx.url}\tController-ish`, {
     'ctx.request.body': JSON.parse(JSON.stringify(ctx.request.body)),
-    hasSessionCookie: ( ctx.state.hasSessionCookie || null),
-    sessionState,
-    sessionState2,
-    sessionState3,
+    hasSessionCookie: ctx.state.hasSessionCookie || null,
+    sessionState: newSessionState,
   })
 })
 
